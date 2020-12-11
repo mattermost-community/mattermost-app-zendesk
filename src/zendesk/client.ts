5a7b3d0fd978422d4b3a2ac46382cb127f47cfad
Jason Frerich <jason.frerich@mattermost.com>
2020-12-11 12:05:59 -0600

    remove object field naming when the same

--- src/zendesk/client.ts
+++ src/zendesk/client.ts
@@ -1,14 +1,14 @@
 import zendesk from 'node-zendesk';
-import { Client, ClientOptions } from 'node-zendesk'
+import {Client, ClientOptions} from 'node-zendesk';
 
 function newClient(username: string, token: string, remoteUri: string): Client {
-  const options: ClientOptions = {
-      username: username,
-      token: token,
-      remoteUri: remoteUri,
-  }
+    const options: ClientOptions = {
+        username,
+        token,
+        remoteUri,
+    };
 
-  return zendesk.createClient(options);
+    return zendesk.createClient(options);
 }
 
 export default newClient;
