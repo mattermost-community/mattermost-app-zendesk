7892e15e166f605c3aa304c319e7a31706de06b0
Jason Frerich <jason.frerich@mattermost.com>
2021-02-15 17:23:46 -0600

    PR feedback spelling

--- src/bindings/channel_header.ts
+++ src/bindings/channel_header.ts
@@ -2,12 +2,12 @@ import {AppBinding} from 'mattermost-redux/types/apps';
 
 import {ZDIcon, Routes, newChannelHeaderBindings, Bindings} from '../utils';
 
-// getChannelHeaderBindings returns the users slash command bindings
+// getChannelHeaderBindings returns the users command bindings
 export const getChannelHeaderBindings = (userID: string): AppBinding => {
     return new ChannelHeaderBindings(userID).getBindings();
 };
 
-// channelHeaderBindings returns bindings for the channeL_header location
+// channelHeaderBindings returns bindings for the channel_header location
 class ChannelHeaderBindings extends Bindings {
     getBindings = (): AppBinding => {
         const bindings: Array<AppBinding> = [];
