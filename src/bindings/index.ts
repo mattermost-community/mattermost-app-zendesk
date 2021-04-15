23e65ae1b2d7396d8c805f87b78e3599681e9bae
Jason Frerich <jason.frerich@mattermost.com>
2021-04-15 11:47:24 -0500

    remove Bindings class

--- src/bindings/index.ts
+++ src/bindings/index.ts
@@ -1,7 +1,5 @@
 import {AppBinding, AppContext} from 'mattermost-redux/types/apps';
 
-import {Bindings} from '../utils';
-
 import {isUserSysadmin} from '../app/user';
 
 import {getCommandBindings} from './slash_commands';
@@ -12,9 +10,9 @@ export async function getBindings(context: AppContext): Promise<AppBinding[]> {
     // prefetch these requests only one time
     const isSysadmin = await isUserSysadmin(context);
 
-    const b = new Bindings(isSysadmin);
-    b.addBindings(getPostMenuBindings(context, isSysadmin));
-    b.addBindings(getCommandBindings(context, isSysadmin));
-    b.addBindings(getChannelHeaderBindings(context, isSysadmin));
-    return b.getAllBindings();
+    const bindings: AppBinding[] = [];
+    bindings.push(getPostMenuBindings(context, isSysadmin));
+    bindings.push(getCommandBindings(context, isSysadmin));
+    bindings.push(getChannelHeaderBindings(context, isSysadmin));
+    return bindings;
 }
--- src/utils/bindings.ts
+++ src/utils/bindings.ts
@@ -4,30 +4,6 @@ import {AppBinding, AppContext} from 'mattermost-redux/types/apps';
 import {CommandTrigger, ZendeskIcon} from '../utils/constants';
 import {getStaticURL} from '../utils';
 
-// Bindings base class stores user info for subclasses
-export class Bindings {
-    sysadmin: boolean
-    private bindings: AppBinding[]
-
-    constructor(isSysadmin: boolean) {
-        this.sysadmin = isSysadmin;
-        this.bindings = [];
-    }
-
-    addBindings = (binding: AppBinding): void => {
-        this.bindings.push(binding);
-    }
-
-    getAllBindings = (): AppBinding[] => {
-        return this.bindings;
-    }
-
-    // check if user is sysadmin through mattermost API
-    isSysadmin = (): boolean => {
-        return this.sysadmin;
-    }
-}
-
 export function newPostMenuBindings(bindings: AppBinding[]): AppBinding {
     const binding = {
         location: AppBindingLocations.POST_MENU_ITEM,
