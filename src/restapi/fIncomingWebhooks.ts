0d02dcdd99cd5c7d9e0a394b3b220287cace759d
Jason Frerich <jason.frerich@mattermost.com>
2021-04-29 02:44:11 -0500

    if user selects status changes, they will get notifications for created   tickets and updated status, open, pending, .etc

--- src/restapi/fIncomingWebhooks.ts
+++ src/restapi/fIncomingWebhooks.ts
@@ -56,19 +56,30 @@ export async function fHandleSubcribeNotification(req: Request, res: Response): 
     res.json({});
 }
 
+// getNotificationMessage returns text for a post message
 async function getNotificationMessage(zdClient: ZDClient, zdUrl: string, ticketID: string, ticketTitle: string, ticketAudit: any): Promise<string> {
+    const ZDTicketPath = Routes.ZD.TicketPathPrefix;
+    const ticketLink = `${zdUrl}${ZDTicketPath}/${ticketID}`;
+    const prefix = `Ticket [#${ticketID}](${ticketLink})  **Subject: [${ticketTitle}](${ticketLink})**  `;
+
     const auditEvents = ticketAudit.events;
     const changeEvents = getEventTypes(auditEvents, 'Change');
-    const idMappedEvents = await getIDMappedTypes(zdClient, changeEvents);
+    if (changeEvents.length) {
+        const events = await mapIDsToTextValues(zdClient, changeEvents);
+        return prefix + getChangedEventText(events);
+    }
 
-    const ZDTicketPath = Routes.ZD.TicketPathPrefix;
-    const ticketLink = `[#${ticketID}](${zdUrl}${ZDTicketPath}/${ticketID})`;
-    const prefix = `Ticket ${ticketLink}  **Subject:** \`${ticketTitle}\` `;
-
-    return prefix + getChangedEventText(idMappedEvents);
+    const createEvents = getEventTypes(auditEvents, 'Create');
+    if (createEvents.length) {
+        const events = await mapIDsToTextValues(zdClient, createEvents);
+        return '**[NEW TICKET]** ' + prefix + getCreatedEventText(events);
+    }
+    throw new Error('Event type does not contain Change or Create event types');
 }
 
-async function getIDMappedTypes(zdClient: ZDClient, events: any[]): Promise<any> {
+// mapIDsToTextValues takes an array of events and maps ID values to human
+// readable text
+async function mapIDsToTextValues(zdClient: ZDClient, events: any[]): Promise<any> {
     const mappedArray: any[] = [];
     const promiseEvents: any[] = [];
 
@@ -82,12 +93,13 @@ async function getIDMappedTypes(zdClient: ZDClient, events: any[]): Promise<any>
             promiseEvents.push(getAssigneeNames(zdClient, event));
             continue;
         }
+        if (event.field_name === 'requester_id') {
+            promiseEvents.push(getAssigneeNames(zdClient, event));
+            continue;
+        }
         if (event.field_name === 'group_id') {
             promiseEvents.push(getGroupNames(zdClient, event));
             continue;
-
-            // promiseEvents.push(getAssigneeNames(zdClient, event));
-            // continue;
         }
         mappedArray.push(event);
     }
@@ -98,65 +110,90 @@ async function getIDMappedTypes(zdClient: ZDClient, events: any[]): Promise<any>
             mappedArray.push(mappedEvent);
         }
     });
-
     return mappedArray;
 }
 
 async function getFormNames(zdClient: ZDClient, event: any): Promise<any> {
-    const prevReq = zdClient.ticketforms.show(event.previous_value);
-    const prevForm = tryPromiseWithMessage(prevReq, 'Failed to fetch previous ticket form');
-
+    const requests: any[] = [];
     const currReq = zdClient.ticketforms.show(event.value);
-    const currForm = tryPromiseWithMessage(currReq, 'Failed to fetch current ticket form');
+    requests.push(tryPromiseWithMessage(currReq, 'Failed to fetch current ticket form'));
 
-    await Promise.all([prevForm, currForm]).then((values) => {
-        event.previous_value = values[0].name;
-        event.value = values[1].name;
+    if (event.previous_value) {
+        const prevReq = zdClient.ticketforms.show(event.previous_value);
+        requests.push(tryPromiseWithMessage(prevReq, 'Failed to fetch previous ticket form'));
+    }
+
+    await Promise.all(requests).then((values) => {
+        event.value = values[0].name;
+        if (values.length === 2) {
+            event.previous_value = values[1].name;
+        }
     });
     return event;
 }
 
 async function getGroupNames(zdClient: ZDClient, event: any): Promise<any> {
-    const prevReq = zdClient.groups.show(event.previous_value);
-    const prevGroup = tryPromiseWithMessage(prevReq, 'Failed to fetch previous group');
-
+    const requests: any[] = [];
     const currReq = zdClient.groups.show(event.value);
-    const currGroup = tryPromiseWithMessage(currReq, 'Failed to fetch current group');
+    requests.push(tryPromiseWithMessage(currReq, 'Failed to fetch current group'));
 
-    await Promise.all([prevGroup, currGroup]).then((values) => {
-        event.previous_value = values[0].name;
-        event.value = values[1].name;
+    if (event.previous_value) {
+        const prevReq = zdClient.groups.show(event.previous_value);
+        requests.push(tryPromiseWithMessage(prevReq, 'Failed to fetch previous group'));
+    }
+
+    await Promise.all(requests).then((values) => {
+        event.value = values[0].name;
+        if (values.length === 2) {
+            event.previous_value = values[1].name;
+        }
     });
     return event;
 }
 
 async function getAssigneeNames(zdClient: ZDClient, event: any): Promise<any> {
-    const prevReq = zdClient.users.show(event.previous_value);
-    const prevUser = tryPromiseWithMessage(prevReq, 'Failed to get previous Zendesk user');
-
+    const requests: any[] = [];
     const currReq = zdClient.users.show(event.value);
-    const currUser = tryPromiseWithMessage(currReq, 'Failed to get current Zendesk user');
+    requests.push(tryPromiseWithMessage(currReq, 'Failed to get current Zendesk user'));
 
-    await Promise.all([prevUser, currUser]).then((values) => {
-        event.previous_value = values[0].name;
-        event.value = values[1].name;
+    if (event.previous_value) {
+        const prevReq = zdClient.users.show(event.previous_value);
+        requests.push(tryPromiseWithMessage(prevReq, 'Failed to get previous Zendesk user'));
+    }
+
+    await Promise.all(requests).then((values) => {
+        event.value = values[0].name;
+        if (values.length === 2) {
+            event.previous_value = values[1].name;
+        }
     });
     return event;
 }
 
+// getEventTypes returns events for a specified event type
 function getEventTypes(auditEvents: any, eventType: string): any {
     return auditEvents.filter((event) => {
         return event.type === eventType;
     });
 }
 
+// getChangedEventText returns text for a changed event types
 function getChangedEventText(events: any[]): string {
     if (events.length === 1) {
         const event = events[0];
         return `\`${event.field_name}\` changed from \`${event.previous_value}\` to \`${event.value}\``;
     }
     const newArray = events.map((event) => {
+        // console.log('event', event);
         return `* \`${event.field_name}\` changed from \`${event.previous_value}\` to \`${event.value}\``;
     });
     return 'The following field values changed \n' + newArray.join('\n');
+}
+
+// getCreatedEventText returns text for a create event types
+function getCreatedEventText(events: any[]): string {
+    const newArray = events.map((event) => {
+        return `* ${event.field_name}: \`${event.value}\``;
+    });
+    return 'A new ticket was created with the following properties: \n' + newArray.join('\n');
 }
