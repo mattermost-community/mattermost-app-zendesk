f920a851f3e2c76737fb041a2b1eba4533778542
Jason Frerich <jason.frerich@mattermost.com>
2021-05-03 15:31:12 -0500

    remove zd_oauth_access_token form config modal Can only update through running the /zd setup-target command by another admin / agent user

--- src/restapi/fConfig.ts
+++ src/restapi/fConfig.ts
@@ -37,8 +37,10 @@ export async function fSubmitOrUpdateZendeskConfigSubmit(req: Request, res: Resp
         const configStore = newConfigStore(context.bot_access_token, context.mattermost_site_url);
         const cValues = await configStore.getValues();
         const targetID = cValues.zd_target_id;
+        const zdOauth2AccessToken = cValues.zd_oauth_access_token;
         const storeValues = call.values as AppConfigStore;
         storeValues.zd_target_id = targetID;
+        storeValues.zd_oauth_access_token = zdOauth2AccessToken;
         configStore.storeConfigInfo(storeValues);
     } catch (err) {
         callResponse = newErrorCallResponseWithMessage(err.message);
