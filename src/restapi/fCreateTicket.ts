13414e8fe7151dba7ad51cf54c38692828fcd043
Jason Frerich <jason.frerich@mattermost.com>
2021-05-13 14:18:16 -0500

    add error handling to create ticket function call handler

--- src/restapi/fCreateTicket.ts
+++ src/restapi/fCreateTicket.ts
@@ -1,34 +1,38 @@
 import {Request, Response} from 'express';
-import {AppCallResponse, AppCallRequest} from 'mattermost-redux/types/apps';
+import {AppCallRequest} from 'mattermost-redux/types/apps';
 
-import {newFormCallResponse, newOKCallResponse, newErrorCallResponseWithMessage} from '../utils/call_responses';
+import {newFormCallResponse, newErrorCallResponseWithMessage} from '../utils/call_responses';
 import {newCreateTicketForm} from '../forms';
 import {newApp} from '../app/app';
 
 // fOpenCreateTicketForm opens a new create ticket form
 export async function fOpenCreateTicketForm(req: Request, res: Response): Promise<void> {
-    const form = await newCreateTicketForm(req.body);
-    const callResponse = newFormCallResponse(form);
-    res.json(callResponse);
+    try {
+        const form = await newCreateTicketForm(req.body);
+        res.json(newFormCallResponse(form));
+    } catch (error) {
+        res.json(newErrorCallResponseWithMessage('Unable to open create ticket form: ' + error.message));
+    }
 }
 
 // fSubmitOrUpdateCreateTicketForm updates the create ticket form with new values or
 // submits the ticket if submit button is clicked
 export async function fSubmitOrUpdateCreateTicketForm(req: Request, res: Response): Promise<void> {
-    const form = await newCreateTicketForm(req.body);
-    const callResponse = newFormCallResponse(form);
-    res.json(callResponse);
+    try {
+        const form = await newCreateTicketForm(req.body);
+        res.json(newFormCallResponse(form));
+    } catch (error) {
+        res.json(newErrorCallResponseWithMessage('Unable to update create ticket form: ' + error.message));
+    }
 }
 
 // fSubmitOrUpdateCreateTicketSubmit creates a ticket
 export async function fSubmitOrUpdateCreateTicketSubmit(req: Request, res: Response): Promise<void> {
     const call: AppCallRequest = req.body;
-    let callResponse: AppCallResponse = newOKCallResponse();
     try {
         const app = newApp(call);
-        callResponse = await app.createTicketFromPost();
+        res.json(await app.createTicketFromPost());
     } catch (err) {
-        callResponse = newErrorCallResponseWithMessage(err.message);
+        res.json(newErrorCallResponseWithMessage(err.message));
     }
-    res.json(callResponse);
 }
