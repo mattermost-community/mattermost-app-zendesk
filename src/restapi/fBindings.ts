e4a3da1337610c40581949e9f6bf554911c31c72
Jason Frerich <jason.frerich@mattermost.com>
2021-05-18 11:43:04 -0500

    fBindings contains no async functions

--- src/restapi/fBindings.ts
+++ src/restapi/fBindings.ts
@@ -4,7 +4,7 @@ import {getBindings} from '../bindings';
 import {CtxExpandedActingUserOauth2AppOauth2User} from '../types/apps';
 import {newOKCallResponseWithData} from '../utils/call_responses';
 
-export async function fBindings(req: Request, res: Response): Promise<void> {
+export function fBindings(req: Request, res: Response): void {
     const context: CtxExpandedActingUserOauth2AppOauth2User = req.body.context;
     const bindings = getBindings(context);
     res.json(newOKCallResponseWithData(bindings));
