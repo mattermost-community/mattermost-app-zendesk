98a3701feb9e6e13727bd7b124f160f6a9536dae
Jason Frerich <jason.frerich@mattermost.com>
2021-08-19 23:00:11 -0500

    add more test cases

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,18 +1,49 @@
-// import {AppSelectOption, AppField, AppCallValues} from 'mattermost-redux/types/apps';
-//
 import {getConditionFieldsFromCallValues} from './utils';
-describe('utils/jira_issue_metadata', () => {
-    test('should return a list of fields', () => {
-        const values = {
-            any_0_field: 'junk',
-        };
 
-        const expected = {
-            0: {
-                field: 'junk',
+describe('utils/getConditionFieldsFromCallValues', () => {
+    test.each([
+        ['one condition',
+            {
+                any_0_field: 'field-0',
+                any_0_operator: 'field-0-oper',
             },
-        };
-        const newValues = getConditionFieldsFromCallValues(values, 'any');
-        expect(newValues).toEqual(expected);
+            {
+                0: {field: 'field-0', operator: 'field-0-oper'},
+            },
+        ],
+        ['two conditions',
+            {
+                any_0_field: 'field-0',
+                any_0_operator: 'field-0-oper',
+                any_1_field: 'field-1',
+            },
+            {
+                0: {field: 'field-0', operator: 'field-0-oper'},
+                1: {field: 'field-1'},
+            },
+        ],
+        ['should ignore state',
+            {
+                state: {state1: 1, state2: 2},
+                any_0_field: 'field-0',
+                any_0_operator: 'field-0-oper',
+            },
+            {
+                0: {field: 'field-0', operator: 'field-0-oper'},
+            },
+        ],
+        ['should ignore any values that are not a condition',
+            {
+                state: {state1: 1, state2: 2},
+                values: {v1: 1, v2: 2, v3: 'XYS'},
+                any_0_field: 'field-0',
+                any_0_operator: 'field-0-oper',
+            },
+            {
+                0: {field: 'field-0', operator: 'field-0-oper'},
+            },
+        ],
+    ])('%s', (_, callVals, expected) => {
+        expect(getConditionFieldsFromCallValues(callVals, 'any')).toEqual(expected);
     });
 });
